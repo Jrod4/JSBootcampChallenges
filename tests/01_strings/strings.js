1fc198002c854c8f9847a1c61571b060c8f96f38
jeffrey <jeffrey.rod93@gmail.com>
2022-10-24 19:22:33 -0400

    strings functions challenge

--- tests/01_strings/strings.js
+++ tests/01_strings/strings.js
@@ -1,19 +1,28 @@
 /* eslint-disable no-unused-vars */
 
 function doesInclude(str, letter) {
-
+  if (str.includes(letter)) {
+    return true;
 }
-
+    return false
+}
 function isPalindrome(str) {
-
+if (str.isPalindrome) {
+  return true
+}
+  return false
 }
 
 function cap(str, letter) {
-
+if (str1[0].toLowercase === str2[0].toLowerCase) {
+  return true;
+}
+  return false;
 }
 
 function firstCharacter(str1, str2) {
-
+const index = str.indexOf(letter);
+console.log(index);
 }
 
 module.exports = {
