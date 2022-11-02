bd810d2cb3104b59514a3a583889e09a42200673
jeffrey <jeffrey.rod93@gmail.com>
2022-11-02 18:58:13 -0400

    arrys function challenge

--- tests/03_arrays/arrays.js
+++ tests/03_arrays/arrays.js
@@ -1,10 +1,22 @@
 /* eslint-disable no-unused-vars */
 function multiply(arr) {
+let total = 1;
 
+const flatArray = arr.flat();
+
+for (let i = 0; i < flatArray.length; i++) {
+  total *= flatArray[i];
+}
+return total; 
 }
 
 function includesCopy(arr, searchValue) {
-
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] === searchValue) {
+    return true;
+  }
+}
+return false;
 }
 
 function inventory(arr) {
